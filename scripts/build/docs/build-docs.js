ee1c29280d12a97d910c951a3ad87e37965aa38f
theajack <1506282385@qq.com>
2022-09-28 22:15:55 +0800

    docs: build-docs

--- scripts/build/docs/build-docs.js
+++ scripts/build/docs/build-docs.js
@@ -3,7 +3,7 @@
  * @Date: 2022-09-28 00:48:05
  * @Description: Coding something
  */
-const {read, write, mkdirDir} = require('../utils');
+const {read, write, mkdirDir, copyFile} = require('../utils');
 
 function modIndexHtmlVersion () {
   const version = process.argv[2] || 'latest';
@@ -11,6 +11,7 @@ function modIndexHtmlVersion () {
   const res = html.match(new RegExp(`https://cdn.jsdelivr.net/npm/disable-devtool@.*/disable-devtool.min.js#use`));
   mkdirDir('@docs');
   if (res) {
+    copyFile('@.gitignore', '@docs/.gitignore')
     write('@docs/index.html', html.replace(res[0], `https://cdn.jsdelivr.net/npm/disable-devtool@${version}/disable-devtool.min.js#use`));
   }
 }
